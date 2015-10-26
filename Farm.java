f2c60bb87d45f81ee2428de7044fe18019cc9597
Kiara Gil <kiarargil@gmail.com>
2015-10-26 11:46:40 -0700

    Update Farm.java

--- Farm.java
+++ Farm.java
@@ -3,7 +3,7 @@ class Farm
 	private Animal[] aBunchOfAnimals = new Animal[3];
 	public Farm()
 	{
-	   	aBunchOfAnimals[0] = new NamedCow("cow", "Elsie", "moo");
+	   	aBunchOfAnimals[0] = new NamedCow("cow", "kiara", "moo");
 	   	aBunchOfAnimals[1] = new Chick("chick", "cluck", "cheep");
 	   	aBunchOfAnimals[2] = new Pig("pig", "oink");
 	}
@@ -12,7 +12,7 @@ class Farm
 		for (int nI = 0; nI < aBunchOfAnimals.length; nI++)
 		{
 			System.out.println(aBunchOfAnimals[nI].getType() + " goes " + aBunchOfAnimals[nI].getSound());
-			System.out.println("The cow is known as " + ((NamedCow)aBunchOfAnimals[0]).getName());
 		}
+		System.out.println("The cow is known as " + ((NamedCow)aBunchOfAnimals[0]).getName());
 	}
 }
